5b843ed370ae524e15ca03e0fe862ee6c1bc2613
lollipopnougat <ab2defg145@gmail.com>
2026-02-07 01:08:29 +0800

    feat: 更新 README 文件，修改项目描述为中文并简化内容；更新 Vite 配置以支持开发环境的基础路径设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+const dev = process.env.NODE_ENV === 'development';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/endfield-grid/',
+  base: dev ? '/' : '/endfield-grid/',
   plugins: [react()]
 });
